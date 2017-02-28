2c0b810de8714516c2475316aa410fb659a23ac5
David Fottrell <davefottrell@gmail.com>
2017-02-28 13:43:25 +0000

    Initial project creation for assignment 3, no code yet, only file structures

---
+++ workspaceComp30670/Assignment3/src/main.py
@@ -0,0 +1,9 @@
+'''
+Created on 28 Feb 2017
+
+@author: David Fottrell
+'''
+
+if __name__ == '__main__':
+    pass    # Need to decide what the call is for this module
+
---
+++ workspaceComp30670/Assignment3/test/test.py
@@ -0,0 +1,15 @@
+'''
+Created on 28 Feb 2017
+
+@author: David Fottrell
+'''
+
+import sys
+from nose.tools import *
+
+# T1 - Test that the program can open, close and read a line from an external file source
+# T2 - Test the first 3 lines read in from the user input file are properly formatted commands 
+# T3 - Test if the command is a switch, skip to T5 if false.  May need a regular expression here!
+# T4 - If T3 is true, test if you have correctly reformatted the array with the toggled LED? e.g. was on, switch it off 
+# T5 - Repeating test 2, count the number of LED's left on and verify the number is correct
+# T6 - Repeat test with deliberately corrupted instructions, e.g. white space, negative array coordinates, etc.
