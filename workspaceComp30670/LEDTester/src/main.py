03a01891993d3f5d2ff3727378c8227d8369d945
David Fottrell <davefottrell@gmail.com>
2017-02-28 18:19:37 +0000

    First Git commit, basic files, no code or tests yet

--- workspaceComp30670/LEDTester/src/main.py
+++ workspaceComp30670/LEDTester/src/main.py
@@ -5,3 +5,18 @@ Created on 28 Feb 2017
 '''
 if __name__ == '__main__':
     pass
+
+def turnOn():
+    # Turn on LED
+    
+def turnOff():
+    #  Turn LED OFF
+    
+def switch():
+    # Toggle LED
+    
+def initArray():
+    # Initialise an array for the LED
+    
+def validCom():
+    # Test if the command 
--- workspaceComp30670/LEDTester/test/test.py
+++ workspaceComp30670/LEDTester/test/test.py
@@ -7,6 +7,7 @@ import sys
 from nose.tools import *
 
 # T1 - Test that the program can open, close and read a line from an external file source
+
 # T2 - Test the first 3 lines read in from the user input file are properly formatted commands 
 # T3 - Test if the command is a switch, skip to T5 if false.  May need a regular expression here!
 # T4 - If T3 is true, test if you have correctly reformatted the array with the toggled LED? e.g. was on, switch it off 
