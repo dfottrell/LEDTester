3b5bc2bdfe3af6befda952788bcecbfb97cdb6a5
David Fottrell <davefottrell@gmail.com>
2017-03-03 15:16:25 +0000

    Updated test.py to reflect class structure of LEDTest in main.py.
    However, unable to get tester to work.  Running out of time to trouble
    shoot this.
    
    Will move on to EC2 instance to test main program instead.

--- workspaceComp30670/LEDTester/test/test.py
+++ workspaceComp30670/LEDTester/test/test.py
@@ -9,11 +9,15 @@ from src.main import LEDTest
 
 N = 5
 a2d = [ [0] * N for _ in range(N) ]    
+
+s1 = 'turn on 0,0 through 5,5'
+s2 = 'turn off 0,0 through 5,5'
+s3 = 'switch 0,0 through 5,5'
     
 def test_validCom():
-    eq_(LEDTest.validCom('turn on 0,0 through 9,9'), 'turnOn(0, 0, 9, 9)', 'validCom "turnOn" call failed')
-    eq_(LEDTest.validCom('turn off 0,0 through 9,9'), 'turnOff(0, 0, 9, 9)', 'validCom "turnOff" call failed')
-    eq_(LEDTest.validCom('switch 0,0 through 9,9'), 'turnOn(0, 0, 9, 9)', 'validCom "switch" call failed')
+    eq_(LEDTest.validCom(a2d, s1), (LEDTest.turnOn(a2d, 0, 0, 5, 5)), "LEDon test failed")
+    eq_(LEDTest.validCom(a2d, s2), (LEDTest.turnOff(a2d, 0, 0, 5, 5)), "LEDOff test failed")
+    eq_(LEDTest.validCom(a2d, s3), (LEDTest.switch(a2d, 0, 0, 5, 5)), "switch test failed")
     
 def test_turnOn():
     eq_(LEDTest.turnOn(0, 0, 1, 1), (sum(a2d) == 2), 'turnOn count failed')
